beb9777509deab6ae160846c1f8438d629b6211f
Dolly Aswin <dolly.aswin@gmail.com>
2018-04-12 13:12:14 +0700

    Initialize Module Set up dependencies

---
+++ Module.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @author Dolly Aswin <dolly.aswin@gmail.com>
+ */
+
+namespace Xtend\Validator;
+
+require __DIR__ . '/src/' . str_replace('\\', '/', __NAMESPACE__) .'/Module.php';
